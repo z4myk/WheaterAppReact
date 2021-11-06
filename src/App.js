ac9f33c0267bf7d863eb8e5b962abc5b81e98864
Sebastian Mosquera <sebimosquera@gmail.com>
2021-11-06 17:13:52 -0300

    se mapeo el json en el dom

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState, useEffect } from "react";
+import "./App.css";
+import Footer from "./components/Footer"
 function App() {
+  const [wheaterData, setWheaterData] = useState({});
+
+  const API_KEY = "b28f2723397ffa83631027439ae40b41";
+  const pais = "Buenos Aires";
+
+  useEffect(() => {
+    wheaterApi();
+  }, []);
+
+  const wheaterApi = async () => {
+    const request = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${pais}&appid=${API_KEY}&units=metric&lang=es`
+    );
+    try {
+      if (request.status === 200) {
+        const response = await request.json();
+        console.log(response);
+        setWheaterData(response);
+        console.log();
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="text-center">
+        <div className="container">
+          <h2 className="mb-5">Wheater App</h2>
+          <div className="cardWheater mx-2">
+            <h4>
+              {wheaterData && wheaterData.name}
+              <span className="badge bg-warning">
+                {wheaterData.sys && wheaterData.sys.country}
+              </span>
+            </h4>
+            <hr className="text-black"/>
+            <p>Temp: {Math.round(wheaterData.main && wheaterData.main.temp)}°C</p>
+
+            <div class="d-flex justify-content-around">
+              <p>
+                {" "}
+                Min: {Math.round(wheaterData.main && wheaterData.main.temp_min)}
+                °C
+              </p>
+              <p>S.T: {Math.round(wheaterData.main && wheaterData.main.feels_like)}°C</p>
+              <p>
+                Max: {Math.round(wheaterData.main && wheaterData.main.temp_max)}
+                °C
+              </p>
+            </div>
+            <p>{wheaterData.wheater && wheaterData.wheater[0].description}</p>
+            <p> Humedad: {wheaterData.main && wheaterData.main.humidity}%</p>
+            <span></span>
+          </div>
+        </div>
+      </div>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const Footer = () => {
+    return (
+        <>
+            <div className="bg-dark p-5 mt-5 pt-5 text-light text-center ">
+            <p>Developed by Sebastian Mosquera with </p>
+            </div>  
+        </>
+    )
+}
+
+export default Footer;
